516cee372e0347d74b22bdc10db23b2d72a8f78e
Bruno <btres76@gmail.com>
2024-01-26 12:41:21 -0300

    adds style to label when selected

--- src/components/molecules/FormAddOrder/FormAddOrder.jsx
+++ src/components/molecules/FormAddOrder/FormAddOrder.jsx
@@ -21,15 +21,26 @@ export default function FormAddOrder() {
             <label className="inside-label" htmlFor="name">Nome</label>
         </fieldset>
         <fieldset>
-          <input type="checkbox" name="monday" id="monday" className="d-none" />
+          
+          
+          
+        
+          
+
           <span className="checkbox-title">Dias</span>
           <div className="label-container">
+            <input type="checkbox" name="monday" id="monday" hidden />
             <label htmlFor="monday" className="checkbox-label">S</label>
-            <label htmlFor="monday" className="checkbox-label">T</label>
-            <label htmlFor="monday" className="checkbox-label">Q</label>
-            <label htmlFor="monday" className="checkbox-label">Q</label>
-            <label htmlFor="monday" className="checkbox-label">S</label>
-            <label htmlFor="monday" className="checkbox-label">S</label>
+            <input type="checkbox" name="tuesday" id="tuesday" hidden />
+            <label htmlFor="tuesday" className="checkbox-label">T</label>
+            <input type="checkbox" name="wednesday" id="wednesday" hidden />
+            <label htmlFor="wednesday" className="checkbox-label">Q</label>
+            <input type="checkbox" name="thursday" id="thursday" hidden />
+            <label htmlFor="thursday" className="checkbox-label">Q</label>
+            <input type="checkbox" name="friday" id="friday" hidden />
+            <label htmlFor="friday" className="checkbox-label">S</label>
+            <input type="checkbox" name="saturday" id="saturday" hidden />
+            <label htmlFor="saturday" className="checkbox-label">S</label>
           </div>
           
         </fieldset>
